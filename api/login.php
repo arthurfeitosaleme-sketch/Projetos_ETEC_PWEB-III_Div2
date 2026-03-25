0ee7fcc1cdc96c9192524b7a043e0715e335ecbd
arthurfeitosaleme <arthurfeitosaleme@gmail.com>
2026-03-25 08:59:39 -0300

    Teste de vulnerabilidade de falhas e correções

--- api/login.php
+++ api/login.php
@@ -1,14 +1,20 @@
 <?php
 require '../app/conexao.php';
 $pdo = Conexao::conectar();
-$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-$json = filter_input(INPUT_GET,'jsn');//{"usuario":"RICKS","senha":"123456"}
-$data = json_decode($json,true);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$json = filter_input(INPUT_GET, 'jsn'); 
+$data = json_decode($json, true);
 $usuario = $data['usuario'];
 $senha = $data['senha'];
-$sql = "select * from usuarios where usulogin = '$usuario' and ususenha = MD5($senha);";
+$sql = "SELECT * FROM usuarios WHERE usulogin = ? AND ususenha = MD5(?)";
 $prp = $pdo->prepare($sql);
 $prp->execute();
-$data = $prp->fetchall(PDO::FETCH_ASSOC);
+$data = $prp->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($data);
 //http://localhost/Projetos_ETEC_PWEB-III_Div2/api/login.php?jsn={"usuario":"XANDAO","senha":"123456"}
+
+//Comando sql inject pra achar vulnerabilidade para inserir
+//http://localhost/Projetos_ETEC_PWEB-III_Div2/api/login.php?jsn={"usuario":"' or '1' = '1'; insert into usuarios (usunome, usulogin, ususenha, usulogado) values ('TUCAO', 'TUCA', '123456', TRUE); --","senha":"123"}
+
+//Comando sql inject pra achar vulnerabilidade para update
+//http://localhost/Projetos_ETEC_PWEB-III_Div2/api/login.php?jsn={"usuario":"' or '1' = '1'; UPDATE usuarios set ususenha = MD5('1234') WHERE usuid = 2 ; --","senha":"123"}
--- api/spusuarios.php
+++ api/spusuarios.php
@@ -5,9 +5,11 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 //$json = $_GET['jsn'];//{"nome":"valor"}
 $json = filter_input(INPUT_GET,'jsn');
 $data = json_decode($json,true);
-$nome = $data['nome'];
-$sql = "select * from usuarios where usunome like '%$nome%';";
+$nome = '%' .$data['nome'] . '%';
+$sql = "select * from usuarios where usunome like ?;";
 $prp = $pdo->prepare($sql);
-$prp->execute();
+$prp->execute(array($nome));
 $data = $prp->fetchall(PDO::FETCH_ASSOC);
 echo json_encode($data);
+
+//http://localhost/Projetos_ETEC_PWEB-III_Div2/api/spusuarios.php?jsn={"nome":"TUCAO"}
