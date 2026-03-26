7306cc38fce14bcd3a1e51ca4b7136a2c9833b3a
ricardoszanata <ricardoszanata@gmail.com>
2026-03-26 12:33:04 -0300

    Criando a carga da listview via api susuarios.php

--- api/login.php
+++ api/login.php
@@ -2,13 +2,13 @@
 require '../app/conexao.php';
 $pdo = Conexao::conectar();
 $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-$json = filter_input(INPUT_GET,'jsn');//{"usuario":"RICKS","senha":"123456"}
+$json = filter_input(INPUT_GET,'jsn');
 $data = json_decode($json,true);
 $usuario = $data['usuario'];
 $senha = $data['senha'];
 $sql = "select * from usuarios where usulogin = ? and ususenha = MD5(?);";
 $prp = $pdo->prepare($sql);
-$prp->execute(array($usuario,$senha));
+$prp->execute([$usuario,$senha]);
 $data = $prp->fetchall(PDO::FETCH_ASSOC);
 echo json_encode($data);
 //http://localhost/Projetos_ETEC_PWEB-III_Div2/api/login.php?jsn={"usuario":"XANDAO","senha":"123456"}
